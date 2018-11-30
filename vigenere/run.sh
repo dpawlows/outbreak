9b4957bdcb7f8eeb79eb3b2f8bd2948cdaa32200
Dave Pawlowski <dpawlows80@gmail.com>
2018-11-30 17:26:24 -0500

    update

---
+++ vigenere/run.sh
@@ -0,0 +1,2 @@
+#echo "IwillsendyoualocationforfurtherinformationviaencryptedemailTounlockusethepasswordCHECKMATE" | java WordCipher -e BLINDFOLD > artichoke.txt
+echo "IT IS TIME.  ADMINISTER M ABC 7:45. GO OSU!" | java WordCipher -e TWELVEHIGHWAY > cortana.txt
