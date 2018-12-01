6bba48ed4cadb7f4114e7292a0d748e40669029a
Dave Pawlowski <dpawlows80@gmail.com>
2018-12-01 12:46:28 -0500

    reordered location and added outbreak sites

--- ascii/run.sh
+++ ascii/run.sh
@@ -1,2 +1,2 @@
 java ASCIICoder 16 "INFECT C AT LOCATION" > message1.txt
-java ASCIICoder 2 "LOUIE AT 8:30" >> message1.txt
+java ASCIICoder 2 "LOCALS AT 8:30" >> message1.txt
--- vigenere/run.sh
+++ vigenere/run.sh
@@ -1,2 +1,2 @@
 #echo "IwillsendyoualocationforfurtherinformationviaencryptedemailTounlockusethepasswordCHECKMATE" | java WordCipher -e BLINDFOLD > artichoke.txt
-echo "IT IS TIME.  ADMINISTER M ABC 7:45. GO OSU!" | java WordCipher -e TWELVEHIGHWAY > cortana.txt
+echo "IT IS TIME.  ADMINISTER M Q+M 7:45. GO OSU!" | java WordCipher -e TWELVEHIGHWAY > cortana.txt
